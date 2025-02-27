24d808e859e95e0c81a2c4de0977f334d8b8f0ae
roshanmetasoft <roshan@metasoftit.com>
2025-02-27 22:16:18 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Newportfolio/",  // 👈 Add this line (your repo name)
   plugins: [react()],
-})
+});
